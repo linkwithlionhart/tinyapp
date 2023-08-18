c7f6f1ed3c7e7366729554653e2f9e559b6ed797
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-18 01:43:12 +0000

    Refactor code

--- express_server.js
+++ express_server.js
@@ -7,12 +7,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// GET request to send a response to the client.
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
+// GET request to send JSON string representing the entire urlDatabase object.
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+// GET request to send HTML code to the client.
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
