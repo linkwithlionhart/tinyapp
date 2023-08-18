fd5de4e364273b51d3d197e2b91d3a343255abd0
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-17 19:22:31 -0700

    Add comments to code

--- express_server.js
+++ express_server.js
@@ -1,40 +1,47 @@
+// Import required modules.
 const express = require('express');
 const app = express();
-const PORT = 8080; // default port 8080
 
+// Define default port for the server.
+const PORT = 8080;
+
+// Database to store shortURLs as keys and their corresponding longURLs as values.
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca", // key: shortURL, value: longURL
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Configure the app to use EJS as the template engine.
 app.set('view engine', 'ejs');
 
-// GET request to send a response to the client.
+// Respond to the root route with a simple greeting.
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
-// GET request to send JSON string representing the entire urlDatabase object.
+// Respond with the entire urlDatabase as a JSON object when requested.
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// GET request to send HTML code to the client.
+// Respond with an HTML page that displays all URLs in the urlDatabase.
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// Respond with details about a specific short URL when provided its ID.
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-// GET request to send HTML code to the client.
+// Respond to the '/hello' route with a simple HTML greeting.
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Start the server and listen on the defined port.
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
