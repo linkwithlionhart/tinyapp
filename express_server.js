2a302ab48a404968b1dc81b7f9a1bca2cd3ec92c
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-27 16:58:48 -0700

    Clean and refactor code

--- express_server.js
+++ express_server.js
@@ -1,18 +1,29 @@
-// 1. Imports
-// Import necessary modules.
+/**
+ * TinyApp server implementation.
+ * Organized into sections:
+ * 1. Imports & Constants
+ * 2. Middleware Configuration
+ * 3. Global Databases & Password Hashing
+ * 4. GET Routes
+ * 5. POST Routes
+ * 6. Server Initialization
+ */
+
+// 1. Imports & Constants
 const express = require('express');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 const morgan = require('morgan');
 const app = express();
-const { getUserByEmail } = require('./helpers');
-
-// 2. Constants and Configuration
-// Set the default port number for the server.
 const PORT = 8080;
+const { 
+  getUserByEmail, 
+  generateRandomString, 
+  getUserByID,
+  urlsForUser 
+} = require('./helpers');
 
-// 3. Middleware
-// Middleware to parse incoming request bodies.
+// 2. Middleware Configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
@@ -20,37 +31,9 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000,
 }));
 app.use(morgan('dev'));
-// Set EJS as the default template engine.
 app.set('view engine', 'ejs');
 
-// 4. Helper Functions
-// Generate random 6-character string.
-const generateRandomString = () => {
-  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let randomString = '';
-  for (let i = 0; i < 6; i++) {
-    randomString += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return randomString;
-};
-// Get user's ID.
-const getUserByID = id => {
-  return users[id];
-}
-
-// Fetch URLs that belong only to user.
-const urlsForUser = id => {
-  let userURLs = {};
-  for (let shortURL in urlDatabase) {
-    if (urlDatabase[shortURL].userID === id) {
-      userURLs[shortURL] = urlDatabase[shortURL];
-    }
-  }
-  return userURLs;
-}
-
-// 5. Databases and Other Global Data Structures
-// Database to store shortURLs as keys and their corresponding longURLs as values.
+// 3. Global Databases & Password Hashing
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -66,7 +49,6 @@ const urlDatabase = {
   },
 };
 
-// Database to store users and their related information.
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -85,34 +67,29 @@ const users = {
   },
 };
 
-// Hash passwords in users database.
 users["userRandomID"].password = bcrypt.hashSync("purple-monkey-dinosaur", 10);
 users["user2RandomID"].password = bcrypt.hashSync("dishwasher-funk", 10);
 users["user3RandomID"].password = bcrypt.hashSync("test", 10);
 
-// 6. Routes
-// Root greeting route.
+// 4. GET Routes
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
-// Simple HTML greeting route.
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Return URL database as JSON.
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// Display all stored URLs.
 app.get('/urls', (req, res) => {
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   if (!user) {
     return res.send("Please log in or register to view URLs.");
   }
-  const userURLs = urlsForUser(user.id);
+  const userURLs = urlsForUser(user.id, urlDatabase);
   const templateVars = { 
     user: user,
     urls: userURLs,
@@ -120,9 +97,8 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-// Display form to create new URL.
 app.get('/urls/new', (req, res) => {
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   const templateVars = {
     user: user,
   }
@@ -132,9 +108,8 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new', templateVars);
 });
 
-// Show details of a specific short URL.
 app.get('/urls/:id', (req, res) => {
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   const shortURL = req.params.id;
   if (!user) {
     return res.send("Please log in to view URL details.");
@@ -153,7 +128,6 @@ app.get('/urls/:id', (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// Redirect from short URL to its corresponding long URL.
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id].longURL;
@@ -163,86 +137,66 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-// Route to login page.
 app.get('/login', (req, res) => {
-  // Fetch the user based on the user_id cookie.
-  const user = getUserByID(req.session.user_id); 
+  const user = getUserByID(req.session.user_id, users); 
   const templateVars = {
     user: user,
   }
-  // Redirect to '/urls' when logged in.
   if (user) {
     return res.redirect('/urls');
   }
   res.render('login', templateVars);
 });
 
-// Route to registration.
 app.get('/register', (req, res) => {
-  // Fetch the user based on the user_id cookie.
-  const user = getUserByID(req.session.user_id); 
+  const user = getUserByID(req.session.user_id, users); 
   const templateVars = {
     user: user,
   }
-  // Redirect to '/urls' when logged in.
   if (user) {
     return res.redirect('/urls');
   }
   res.render('register', templateVars);
 });
 
-// Registration handler: Route to registration endpoint that handles registration form data.
+// 5. POST Routes
 app.post('/register', (req, res) => {
-  // Extract email and password from request body.
   const { email, password } = req.body;
-  // Check if email and password are provided.
   if (!email || !password) {
     return res.status(400).send('You must provide both an email and a password.');
   }
-  // Check if user with email already exists.
   const existingUser = getUserByEmail(email, users);
   if (existingUser) {
     return res.status(400).send("Email already exists. Proceed to login or try again.");
   }
-  // Generate random ID for user.
   const id = generateRandomString();
-  // Hash the password
   const hashedPassword = bcrypt.hashSync(password, 10);
-  // Create new user object.
   users[id] = {
     id, 
     email,
     password: hashedPassword,
   }
-  // Set user id in session
   req.session.user_id = id;
-  // Redirect to /urls
   res.redirect('/urls');
 });
 
-// Route login endpoint to set the user cookie and redirect.
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  // Find user based on email.
   const user = getUserByEmail(email, users);
   if (!user || !bcrypt.compareSync(password, user.password)) {
     return res.status(403).send("Email or password is incorrect.");
   }
-  // On successful login, set user_id cookie and redirect to '/urls'
   req.session.user_id = user.id;
   res.redirect('/urls');
 });
 
-// Route logout endpoint to clear the user cookie and redirect to '/urls'
 app.post('/logout', (req, res) => {
   delete req.session.user_id;
   res.redirect('/login');
 });
 
-// Add new short and long URL to the database.
 app.post('/urls', (req, res) => {
-  // Redirect users not logged in.
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   if (!user) {
     return res.status(403).send("You must be logged in to shorten a URL.");
   }
@@ -254,11 +208,9 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Update a specific short URL's corresponding long URL.
 app.post('/urls/:id/update', (req, res) => {
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   const shortURL = req.params.id;
-  // Redirect users not logged in.
   if (!user) {
     return res.status(403).send("You must be logged in to edit long URLs.");
   }
@@ -272,11 +224,9 @@ app.post('/urls/:id/update', (req, res) => {
   res.redirect('/urls');  
 });
 
-// Delete a short URL from the database.
 app.post('/urls/:id/delete', (req, res) => {
-  const user = getUserByID(req.session.user_id);
+  const user = getUserByID(req.session.user_id, users);
   const shortURL = req.params.id;
-  // Redirect users not logged in.
   if (!user) {
     return res.status(400).send("You must be logged in to delete URLs.");
   }
@@ -290,8 +240,7 @@ app.post('/urls/:id/delete', (req, res) => {
   res.redirect('/urls');
 });
 
-// 7. Server Initialization
-// Start the server.
+// 6. Server Initialization
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
 });
