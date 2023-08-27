75f303dca91cf2beba29d6d85d9ca990bfd6d92f
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-27 16:59:51 -0700

    Moved other helper functions into helpers module

--- helpers.js
+++ helpers.js
@@ -13,4 +13,46 @@ const getUserByEmail = (email, database) => {
   return null;
 };
 
-module.exports = { getUserByEmail };
+/**
+ * Generates a random string of 6 characters using alphanumeric characters.
+ * @returns {string} A randomly generated string.
+ */
+const generateRandomString = () => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    randomString += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return randomString;
+};
+
+/**
+ * Retrieves a user object from the users database using the given ID.
+ * @param {string} id - The ID of the user to be retrieved.
+ * @returns {object} The user object, or undefined if the user with the given ID does not exist.
+ */
+const getUserByID = (id, users) => {
+  return users[id];
+};
+
+/**
+ * Retrieves all URLs associated with a specific user ID from the URL database.
+ * @param {string} id - The ID of the user whose URLs are to be retrieved.
+ * @returns {object} An object containing all the short URLs associated with the user and their details.
+ */
+const urlsForUser = (id, urlDatabase) => {
+  let userURLs = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
+};
+
+module.exports = { 
+  getUserByEmail, 
+  generateRandomString, 
+  getUserByID,
+  urlsForUser,
+};
